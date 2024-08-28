72f0c8016f7582948ec8a1e45d35583374c388b2
Anmol Sharma <136828626+Anmol-Shh@users.noreply.github.com>
2024-08-28 22:46:44 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,116 @@
+let searchBtn = document.querySelector('#search-btn');
+let searchBar = document.querySelector('.search-bar-container');
+let formBtn = document.querySelector('#login-btn');
+let loginForm = document.querySelector('.login-form-container');
+let formClose = document.querySelector('#form-close');
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+let videoBtn = document.querySelectorAll(".vid-btn");
+
+
+
+
+window.onscroll = () => {
+    searchBtn.classList.remove('fa-times');
+    searchBar.classList.remove('active');
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+    
+};
+
+menu.addEventListener('click',() => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+});
+
+searchBtn.addEventListener('click',() => {
+    searchBtn.classList.toggle('fa-times');
+    searchBar.classList.toggle('active');
+});
+
+formBtn.addEventListener('click',() => {
+    loginForm.classList.add('active');
+});
+
+formClose.addEventListener('click',() => {
+    loginForm.classList.remove('active');
+});
+
+videoBtn.forEach(btn => {
+    btn.addEventListener('click', ()=>{
+        document.querySelector('.controls .active').classList.remove('active');
+        btn.classList.add('active');
+        let src = btn.getAttribute('data-src');
+        document.querySelector('#video-slider').src = src;
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    var swiper = new Swiper(".review-slider", {
+    spaceBetween:20,
+    loop:true,
+    autoplay:{
+        delay:2500,
+        disableOnInteraction:false,
+    },
+    breakpoints:{
+        640:{
+            slidesPerView:1,
+        },
+        768:{
+            slidesPerView:2,
+        },
+        1024:{
+            slidesPerView:3,
+        },
+    },
+    })
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    var swiper = new Swiper(".brand-slider", {
+    spaceBetween:20,
+    loop:true,
+    autoplay:{
+        delay:2500,
+        disableOnInteraction:false,
+    },
+    breakpoints:{
+        400:{
+            slidesPerView:2,
+        },
+        500:{
+            slidesPerView:3,
+        },
+        768:{
+            slidesPerView:4,
+        },
+        1024:{
+            slidesPerView:5,
+        },
+    },
+    })
+});
+
+
+  
+// document.addEventListener('DOMContentLoaded', function () {
+//     var swiper = new Swiper('.mySwiper', {
+//         direction: 'horizontal',
+//         slidesPerView: 1,
+//         accessibility: true,
+//         loop: true,
+//         pagination: {
+//             el: '.swiper-1 .swiper-pagination',
+//             clickable: true
+//         },
+//         autoplay: { // This will automatically slide
+//             delay: 2500,
+//             disableOnInteraction: false,
+//         },
+//         navigation: {
+//             nextEl: '.swiper-button-next',
+//             prevEl: '.swiper-button-prev',
+//         },
+//     });
+// });
